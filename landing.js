ede2eb438374c9e91aff7373f21b91e326308004
StanzTheDev <135062697+StanzTheDev@users.noreply.github.com>
2024-06-09 02:58:19 -0700

    Add files via upload

---
+++ landing.js
@@ -0,0 +1,12 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+  }
+  function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+  }
+   document.querySelector('#bout').addEventListener('click', ()=>{
+document.querySelector('.dis').style.animation = 'slideInFromLeft 1s ease'
+   });
+    
